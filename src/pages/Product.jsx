47ec4b8658f1de49821ddce052d167fc3e1fbdb2
Guillaume Sadourny <guillaume.sadourny@ecoles-epsi.net>
2025-02-21 15:22:19 +0100

    feat: complete cartPage and Place Order function

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -82,7 +82,7 @@ const Product = () => {
           </div>
           <button
             onClick={() => addToCart(productData._id, size)}
-            className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700"
+            className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700 cursor-pointer"
           >
             ADD TO CART
           </button>
