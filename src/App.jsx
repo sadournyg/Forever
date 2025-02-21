9c542af197e56bc2ba197bd00d882854f1f4d1e8
Guillaume Sadourny <guillaume.sadourny@ecoles-epsi.net>
2025-02-21 08:36:58 +0100

    correct Routes

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,18 @@ import Cart from "./pages/Cart";
 import Login from "./pages/Login";
 import PlaceOrder from "./pages/PlaceOrder";
 import Orders from "./pages/Orders";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/collection" element={<Collection />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/product:productId" element={<Product />} />
+        <Route path="/product/:productId" element={<Product />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/login" element={<Login />} />
         <Route path="/place-order" element={<PlaceOrder />} />
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+import { assets } from "../assets/assets";
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between py-5 font-medium"></div>
+  );
+};
+
+export default Navbar;
